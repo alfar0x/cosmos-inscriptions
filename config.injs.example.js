55b1a29cba4870ce670286589af7422c74604f56
John Doe <johndoe@example.com>
2023-12-19 14:27:15 +0200

    minor

---
+++ config.injs.example.js
@@ -0,0 +1,43 @@
+// common
+export const RPC = "https://injective-rest.publicnode.com/"; // ??????????????
+export const NATIVE_PRICE_USD = 39.25;
+
+// mint
+export const FEE = 10000; // ??????????????
+export const GAS = 100000; // ??????????????
+export const MINT_AMOUNT_UNATIVE = 300; // ??????????????
+
+export const SLEEP_ON_GET_ACCOUNT_ERROR_SEC = 20;
+export const SLEEP_BETWEEN_START_ACCOUNTS_SEC = 5;
+export const SLEEP_BETWEEN_ACCOUNT_TXS_SEC = 5;
+
+// dispatch
+export const SEND_NATIVE_TOKENS_PER_ACCOUNT = 0.4;
+export const SLEEP_BETWEEN_DISPATCH_SEC = 30;
+
+// withdraw
+export const LEAVE_NATIVE_ON_ACCOUNT = 0.05;
+export const SLEEP_BETWEEN_WITHDRAW_SEC = 5;
+
+// balacnes
+export const SLEEP_BETWEEN_CHECK_BALANCES_SEC = 30;
+
+// transactions
+export const SLEEP_BETWEEN_GET_TRANSACTIONS_SEC = 5;
+export const TXS_API_URL = null;
+
+// minted
+export const SLEEP_BETWEEN_CHECK_MINTED_SEC = 5;
+export const MINTED_API_URL = null;
+
+// system dont edit
+export const EXPLORER = "https://www.mintscan.io/injective/tx";
+export const MEMO =
+  "ZGF0YToseyJwIjoiaW5qcmMtMjAiLCJvcCI6Im1pbnQiLCJ0aWNrIjoiSU5KUyIsImFtdCI6IjIwMDAifQ==";
+export const NATIVE_DENOM = "uinj"; // ??????????????
+export const FILE_ACCOUNTS = "input/accounts.txt";
+export const UNATIVE_PER_NATIVE = 10 ** 18;
+export const NATIVE_TICK = "INJ";
+export const ADDRESS_LENGTH = 42;
+export const ADDRESS_PREFIX = "inj";
+export const WITHDRAW_EXCHANGE_ADDRESS = "inj....";
